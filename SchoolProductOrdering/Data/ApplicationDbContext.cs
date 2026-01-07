5a6af182bded93184f492dee32e607101ca411a9
nkunamphogelo,-lab <nkunamphogelo@gmail.com>
2026-01-07 14:26:28 +0200

    created an productordering app, added  model and data folder also changed the database Connection

---
+++ SchoolProductOrdering/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolProductOrdering.Models;
+
+namespace SchoolProductOrdering.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
