2aba39eda53ea9046b8353f0d79112e36aeb5b9c
nkunamphogelo,-lab <nkunamphogelo@gmail.com>
2026-01-07 12:14:11 +0200

    Add project files.

---
+++ SchoolProductOrdering/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolProductOrdering.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        public string? ImagePath { get; set; }
+    }
+}
