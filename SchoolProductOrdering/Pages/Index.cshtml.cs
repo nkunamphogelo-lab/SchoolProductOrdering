34b1cef9937c8ac9e9d30a9affe78eac8f3fe2b9
nkunamphogelo,-lab <nkunamphogelo@gmail.com>
2026-01-09 14:15:17 +0200

    Add Navigation Link

--- SchoolProductOrdering/Pages/Index.cshtml.cs
+++ SchoolProductOrdering/Pages/Index.cshtml.cs
@@ -1,20 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SchoolProductOrdering.Data;
+using SchoolProductOrdering.Models;
 
 namespace SchoolProductOrdering.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly SchoolProductOrdering.Data.ApplicationDbContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(SchoolProductOrdering.Data.ApplicationDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
-        {
+        public IList<Product> Product { get;set; } = default!;
 
+        public async Task OnGetAsync()
+        {
+            Product = await _context.Products.ToListAsync();
         }
     }
 }
